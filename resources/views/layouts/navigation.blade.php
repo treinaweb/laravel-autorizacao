458af67076c3d6e9ad6adf141e42970e2510d409
Elton Fonseca <elton869@gmail.com>
2024-03-22 13:03:50 -0300

    08 - Preparando nosso aplicação para os próximos conceitos

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,12 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('funcionarios.index')" :active="request()->routeIs('funcionarios')">
+                        Funcionários
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
@@ -69,6 +75,12 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+        </div>
+
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('funcionarios.index')" :active="request()->routeIs('funcionarios')">
+                Funcionários
             </x-responsive-nav-link>
         </div>
 
